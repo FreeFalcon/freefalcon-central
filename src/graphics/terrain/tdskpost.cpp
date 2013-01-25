167959dde38fa1234226411b685f243f45e4fea9
Vinny Diehl <gbchaosmaster926@gmail.com>
2013-01-24 23:31:51 -0500

    Clean up tdskpost.cpp

--- src/graphics/terrain/tdskpost.cpp
+++ src/graphics/terrain/tdskpost.cpp
@@ -1,5 +1,3 @@
-// Optimizer bad, debug good
-// #pragma optimize( "", off )
 /***************************************************************************\
     TDskPost
     November 13, 1995
@@ -11,14 +9,15 @@
 
  This is version that is used to store the map in the disk file.
 \***************************************************************************/
+
 #include "Tpost.h"
 #include "TdskPost.h"
 #include "TMap.h"
 
-//void DiskblockToMemblock( Tpost *memPost, TdiskPost *diskPost, int LOD, float lightLevel, float *minZ, float *maxZ )
-void DiskblockToMemblock(Tpost *memPost, TdiskPost *diskPost, int LOD, float, float *minZ, float *maxZ)
+void DiskblockToMemblock(Tpost *memPost, TdiskPost *diskPost,
+                         int LOD, float,
+                         float *minZ, float *maxZ)
 {
-    int i;
     float minZvalue = 1e6f;
     float maxZvalue = -1e6f;
 
@@ -29,9 +28,8 @@ void DiskblockToMemblock(Tpost *memPost, TdiskPost *diskPost, int LOD, float, fl
     ShiAssert(g_LargeTerrainFormat == false);
 
     // Convert the data from disk format to our current runtime format
-    for (i = 0; i < POSTS_PER_BLOCK; i++)
+    for (int i = 0; i < POSTS_PER_BLOCK; ++i)
     {
-
         // Scale from integer feet (Z up) to floating point feet (Z down)
         memPost->z = -(float)(diskPost->z);
 
@@ -41,26 +39,13 @@ void DiskblockToMemblock(Tpost *memPost, TdiskPost *diskPost, int LOD, float, fl
         // The "& 0x3" and 0.25 terms are because we have 4 posts (0,1,2,3) accross each
         // texture at LOD 0.
 
-#if 0 // This way burns a pixel at each edge to improve bi-linear filtering without clamp mode.
-        static const float start = 1.0f / 128.0f;
-#else // This way uses the whole texture -- requires clamp mode for bilinear filtering.
         static const float start = 0.00001f;
-#endif
-
         static const float stop = 1.0f - start;
         static const float minStep = (stop - start) * 0.25f;
 
         memPost->u = start + ((i << LOD) & 0x3) * minStep;
         memPost->v = stop - (((i >> POST_OFFSET_BITS) << LOD) & 0x3) * minStep;
-
-        if (LOD < TheMap.LastNearTexLOD())
-        {
-            memPost->d = (1 << LOD) * minStep;
-        }
-        else
-        {
-            memPost->d = stop;
-        }
+        memPost->d = LOD < TheMap.LastNearTexLOD() ? (1 << LOD) * minStep : stop;
 
         // Copy the color index
         memPost->colorIndex = diskPost->color;
@@ -78,28 +63,18 @@ void DiskblockToMemblock(Tpost *memPost, TdiskPost *diskPost, int LOD, float, fl
         static const double phiOutScale = 63.99;
         memPost->phi = (float)((diskPost->phi * phiInRange / phiOutScale) + phiInStart);
 
-        /* ShiAssert( memPost->theta > -0.001f );
-         ShiAssert( memPost->theta < 2.00f * PI + 0.001f );
-         ShiAssert( memPost->phi > -0.001f );
-         ShiAssert( memPost->phi < PI / 2.0f + 0.001f );
-        */
         // Request the texture used by this post (if any)
         if (LOD <= TheMap.LastNearTexLOD())
-        {
             TheTerrTextures.Request(memPost->texID);
-        }
-        else
-        {
-            TheFarTextures.Request(memPost->texID);
-        }
+        else TheFarTextures.Request(memPost->texID);
 
         // Accumulate the min and max Z values (These could be stored in the block on disk)
         minZvalue = min(minZvalue, memPost->z);
         maxZvalue = max(maxZvalue, memPost->z);
 
         // Move on to the next post
-        memPost++;
-        diskPost++;
+        ++memPost;
+        ++diskPost;
     }
 
     *minZ = minZvalue;
@@ -110,7 +85,6 @@ void DiskblockToMemblock(Tpost *memPost, TdiskPost *diskPost, int LOD, float, fl
 // have time now to check it all out.
 void LargeDiskblockToMemblock(Tpost *memPost, TNewdiskPost *diskPost, int LOD, float, float *minZ, float *maxZ)
 {
-    int i;
     float minZvalue = 1e6f;
     float maxZvalue = -1e6f;
 
@@ -121,39 +95,24 @@ void LargeDiskblockToMemblock(Tpost *memPost, TNewdiskPost *diskPost, int LOD, f
     ShiAssert(g_LargeTerrainFormat == true);
 
     // Convert the data from disk format to our current runtime format
-    for (i = 0; i < POSTS_PER_BLOCK; i++)
+    for (int i = 0; i < POSTS_PER_BLOCK; ++i)
     {
-
         // Scale from integer feet (Z up) to floating point feet (Z down)
         memPost->z = -(float)(diskPost->z);
 
-        // Pu239 fix error of assigning uint to DWORD
         memPost->texID = (DWORD) diskPost->texID;
 
         // Compute the texture coordinates for this post
         // The "& 0x3" and 0.25 terms are because we have 4 posts (0,1,2,3) accross each
         // texture at LOD 0.
 
-#if 0 // This way burns a pixel at each edge to improve bi-linear filtering without clamp mode.
-        static const float start = 1.0f / 128.0f;
-#else // This way uses the whole texture -- requires clamp mode for bilinear filtering.
         static const float start = 0.00001f;
-#endif
-
         static const float stop = 1.0f - start;
         static const float minStep = (stop - start) * 0.25f;
 
         memPost->u = start + ((i << LOD) & 0x3) * minStep;
         memPost->v = stop - (((i >> POST_OFFSET_BITS) << LOD) & 0x3) * minStep;
-
-        if (LOD < TheMap.LastNearTexLOD())
-        {
-            memPost->d = (1 << LOD) * minStep;
-        }
-        else
-        {
-            memPost->d = stop;
-        }
+        memPost->d = LOD < TheMap.LastNearTexLOD() ? (1 << LOD) * minStep : stop;
 
         // Copy the color index
         memPost->colorIndex = diskPost->color;
@@ -171,28 +130,18 @@ void LargeDiskblockToMemblock(Tpost *memPost, TNewdiskPost *diskPost, int LOD, f
         static const double phiOutScale = 63.99;
         memPost->phi = (float)((diskPost->phi * phiInRange / phiOutScale) + phiInStart);
 
-        /* ShiAssert( memPost->theta > -0.001f );
-         ShiAssert( memPost->theta < 2.00f * PI + 0.001f );
-         ShiAssert( memPost->phi > -0.001f );
-         ShiAssert( memPost->phi < PI / 2.0f + 0.001f );
-        */
         // Request the texture used by this post (if any)
         if (LOD <= TheMap.LastNearTexLOD())
-        {
             TheTerrTextures.Request(memPost->texID);
-        }
-        else
-        {
-            TheFarTextures.Request(memPost->texID);
-        }
+        else TheFarTextures.Request(memPost->texID);
 
         // Accumulate the min and max Z values (These could be stored in the block on disk)
         minZvalue = min(minZvalue, memPost->z);
         maxZvalue = max(maxZvalue, memPost->z);
 
         // Move on to the next post
-        memPost++;
-        diskPost++;
+        ++memPost;
+        ++diskPost;
     }
 
     *minZ = minZvalue;
