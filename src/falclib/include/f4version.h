72c0e995c04b0b2b32ba2a590f6a5284c03a89aa
dannycoh <dannycoh@gmail.com>
2014-10-09 12:31:11 +0300

    Revert "Another fix for the version number"
    This reverts commit 622c931033c5639195209dc5d07c94f7054229a8.

--- src/falclib/include/f4version.h
+++ src/falclib/include/f4version.h
@@ -1,25 +1,18 @@
 #ifndef _FALCON4_VERSION_H
 #define _FALCON4_VERSION_H
 
-// dannycoh -removed f4VerNum.h and changed version to reflect FFOSP7.
-// dannycoh -removed because it is shown only during build.
-//const int FfMajorVersion = 7;
-//const int FfMinorVersion = 0;
-//const int FfLanguage = 1;
-//const int FfBuildNumber = 0;
-//#define FfLanguageAbbreviated "US"
-//#define FfBuildType "DEBUG "
+#include "f4VerNum.h"
 
-//#define STRINGERL(A) #A
-//#define STRINGER(A) STRINGERL(A)
-//#define Charmakerl(A) #@A
-//#define Charmaker(A) Charmakerl(A)
+#define STRINGERL(A) #A
+#define STRINGER(A) STRINGERL(A)
+#define Charmakerl(A) #@A
+#define Charmaker(A) Charmakerl(A)
 
-//#define VERSION_COMMSTRING    "FreeFalcon 7.0 " FfBuildType "(" FfLanguageAbbreviated ")\0"
-//#define VERSION_FILEDESC      "FreeFalcon 7.0 " FfBuildType "(" FfLanguageAbbreviated ")\0"
-//#define VERSION_FILEVERSION    STRINGER(FfMajorVersion) ",0," STRINGER(FfMinorVersion) "," STRINGER (FfLanguage)
-//#define VERSION_PRODUCTVERSION STRINGER(FfMajorVersion) ".0" STRINGER(FfMinorVersion) STRINGER (FfLanguage)
-//#define VERSION_SPECIAL        FfBuildType FfLanguageAbbreviated " Edition"
+#define VERSION_COMMSTRING    "FreeFalcon 6.1 " F4BuildType "(" F4LanguageAbbrev ")\0"
+#define VERSION_FILEDESC      "FreeFalcon 6.1 " F4BuildType "(" F4LanguageAbbrev ")\0"
+#define VERSION_FILEVERSION    STRINGER(F4MajorVersion) ",0," STRINGER(F4MinorVersion) "," STRINGER (F4Language)
+#define VERSION_PRODUCTVERSION STRINGER(F4MajorVersion) ".0" STRINGER(F4MinorVersion) STRINGER (F4Language)
+#define VERSION_SPECIAL        F4BuildType F4LanguageAbbrev " Edition"
 
 #define F4LANG_ENGLISH 1
 #define F4LANG_UK 2
@@ -33,11 +26,10 @@
 #define F4LANG_FEMININE 1
 #define F4LANG_NEUTER 2
 
-// dannycoh -removed because it is shown only during build.
-//#pragma message ( VERSION_FILEDESC )
-//#pragma message ( VERSION_FILEVERSION )
-//#pragma message ( VERSION_PRODUCTVERSION )
-//#pragma message ( VERSION_SPECIAL )
+#pragma message ( VERSION_FILEDESC )
+#pragma message ( VERSION_FILEVERSION )
+#pragma message ( VERSION_PRODUCTVERSION )
+#pragma message ( VERSION_SPECIAL )
 
 extern int gLangIDNum;
 
